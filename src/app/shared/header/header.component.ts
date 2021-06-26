5f0c3024ae1de93d865e0ba2ad2e92d72e38b014
Bruna A Braguin <bruna.braguin@keyrus.com.br>
2021-06-26 20:16:08 -0300

    Get search param

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  searchParam: FormControl = new FormControl('');
+  sendParam = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onSearchPokemon(){
+    const pokemonIDName = (this.searchParam.value).trim().toLowerCase();
+  };
 }
