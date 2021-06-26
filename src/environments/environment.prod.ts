f9d4750dc33f7502f3e6406c8ed261aaba0d68ec
Bruna A Braguin <bruna.braguin@keyrus.com.br>
2021-06-26 00:37:46 -0300

    Add type icons

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,6 @@ export const environment = {
   pokeApiURL: 'https://pokeapi.co/api/v2/',
   pokemonImageURL:
     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/',
+  pokemonType:
+    'https://commons.wikimedia.org/wiki/Category:Pok%C3%A9mon_types_icons',
 };
