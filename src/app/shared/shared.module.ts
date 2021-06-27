b65bb5622aee21ddb90f203581ff66ea1b4a14c5
Bruna A Braguin <bruna.braguin@keyrus.com.br>
2021-06-27 02:03:31 -0300

    Populate Pokemon Details

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './../material.module';
 import { CommonModule } from '@angular/common';
 import { PokemonListComponent } from '../components/pokemon-list/pokemon-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [PokemonListComponent],
-  imports: [MaterialModule, CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    MaterialModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   exports: [PokemonListComponent],
 })
 export class SharedModule {}
